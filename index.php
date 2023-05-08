445eb37f145d1e8a1a87adc7a0d89f912b80a4bb
Paolo Giovannini <giovannini13@gmail.com>
2023-05-08 16:18:56 +0200

    ulteriore correzione exception

--- index.php
+++ index.php
@@ -13,19 +13,13 @@ $prodotti = [
 
 try {
     $cuccia1 ->setPrezzo(30);
-} catch (Exception $e) {
-    $errore = $e->getMessage();
-}
+} catch (Exception $e) {}
 try {
     $cibo1 ->setPrezzo(10);
-} catch (Exception $e){
-    $errore = $e->getMessage();
-}
+} catch (Exception $e){}
 try {
     $gioco1 ->setPrezzo(20);
-} catch (Exception $e) {
-    $errore = $e->getMessage();
-}
+} catch (Exception $e) {}
 
 
 
@@ -56,7 +50,7 @@ try {
                             <p>Prezzo: <?php if(is_int($prodotto-> prezzo)){
                              echo $prodotto->prezzo; 
                              }else{
-                                echo "Si è verificato un errore sul prodotto: " . $errore;
+                                echo "Si è verificato un errore sul prodotto: " . $e->getMessage();
                              } ?> </p>
                             
                             <p>Categoria: <?php echo $prodotto->categoria ?> </p>
